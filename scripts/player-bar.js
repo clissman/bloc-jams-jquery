6d7f1f40c83bcee1ea102491f07cace5db188244
Chris <clissmanux@gmail.com>
2019-09-19 16:12:27 -0600

    Complete Checkpoint 12.18 Sliders

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -17,7 +17,19 @@
 
         const newSong = album.songs[newSongIndex]
         player.playPause(newSong)
-
     }
+
+    $('#time-control input').on('input', e => {
+        player.skipTo(e.target.value);
+    })
+
+    setInterval(() => {
+        if (player.playState !== 'playing') { return; }
+        const currentTime = player.getTime();
+        const duration = player.getDuration();
+        const percent = (currentTime / duration) * 100;
+        $('#time-control .current-time').text(currentTime);
+        $('#time-control input').val(percent);
+    }, 1000);
 }
 
